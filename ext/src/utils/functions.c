87852729e886cb1f794293d3f2fc5c0779d4329f
Roman Konovaltsev <konovaltsev@stroytorgi.ru>
2013-12-04 11:53:06 +0400

    matrix_mul

--- ext/src/utils/functions.c
+++ ext/src/utils/functions.c
@@ -95,6 +95,112 @@ int php_matrix_init_matrix(ZMatrix* out, zval* php_matrix)
     return SUCCESS;
 }
 
+int php_matrix_init_matrix_double(DMatrix* out, zval* php_matrix)
+{
+    zval **data, **data2;
+    int n, m, i, j, idx, i_mem;
+    HashTable *j_hash, *i_hash;
+    HashPosition pointer, pointer2;
+    double *matrix;
+
+    zval temp;
+
+    out->n = 0;
+    out->m = 0;
+    out->matrix = NULL;
+
+    if (Z_TYPE_P(php_matrix) != IS_ARRAY)
+    {
+        return FAILURE;
+    }
+
+    i_hash = Z_ARRVAL_P(php_matrix);
+    zend_hash_internal_pointer_reset_ex(i_hash, &pointer);
+    m = zend_hash_num_elements(i_hash);
+    if(zend_hash_get_current_data_ex(i_hash, (void**) &data, &pointer) == SUCCESS)
+    {
+        j_hash = Z_ARRVAL_PP(data);
+        n = zend_hash_num_elements(j_hash);
+    }
+    else
+    {
+        return FAILURE;
+    }
+    matrix = emalloc(sizeof(*(out->matrix)) * m * n);
+
+    idx = 0;
+    for(
+        i = 0, zend_hash_internal_pointer_reset_ex(i_hash, &pointer);
+        i < m;
+        ++i, zend_hash_move_forward_ex(i_hash, &pointer)
+    )
+    {
+        if
+        (
+            zend_hash_get_current_data_ex(i_hash, (void**) &data, &pointer) != SUCCESS
+            ||
+            Z_TYPE_PP(data) != IS_ARRAY
+        )
+        {
+            efree(matrix);
+            return FAILURE;
+        }
+
+        j_hash = Z_ARRVAL_PP(data);
+        if(zend_hash_num_elements(j_hash) != n)
+        {
+            efree(matrix);
+            return FAILURE;
+        }
+
+        for(
+            j = 0, zend_hash_internal_pointer_reset_ex(j_hash, &pointer2);
+            j < n;
+            ++j, zend_hash_move_forward_ex(j_hash, &pointer2)
+        )
+        {
+            if(zend_hash_get_current_data_ex(j_hash, (void**) &data2, &pointer2) != SUCCESS)
+            {
+                efree(matrix);
+                return FAILURE;
+            }
+
+            temp = **data2;
+            zval_copy_ctor(&temp);
+            convert_to_double(&temp);
+            matrix[idx] = Z_DVAL(temp);
+            zval_dtor(&temp);
+
+            ++idx;
+        }
+    }
+
+    out->n = n;
+    out->m = m;
+    out->matrix = matrix;
+
+    return SUCCESS;
+}
+
+int php_dmatrix_to_pzval(DMatrix matrix, zval* ret)
+{
+    int i, j, idx = 0;
+    zval *row;
+
+    array_init(ret);
+    for(i = 0; i < matrix.m; ++i)
+    {
+        MAKE_STD_ZVAL(row);
+        array_init(row);
+        for(j = 0; j < matrix.n; ++j)
+        {
+            add_next_index_double(row, matrix.matrix[idx]);
+            ++idx;
+        }
+        add_next_index_zval(ret, row);
+    }
+}
+
 int php_matrix_init_matrix_long(LMatrix* out, zval* php_matrix)
 {
     zval **data, **data2;
@@ -126,7 +232,7 @@ int php_matrix_init_matrix_long(LMatrix* out, zval* php_matrix)
     {
         return FAILURE;
     }
-    matrix = emalloc(sizeof(*matrix) * m * n);
+    matrix = emalloc(sizeof(*(out->matrix)) * m * n);
 
     idx = 0;
     for(
