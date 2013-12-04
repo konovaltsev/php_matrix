e88d9c5e5a43a533aa89bf7b2b7090641cb946ee
Roman Konovaltsev <konovaltsev@stroytorgi.ru>
2013-12-04 14:09:13 +0400

    matrix_version() example

--- example/matrix_version.php
+++ example/matrix_version.php
@@ -1,14 +1,2 @@
 <?php
 var_dump(matrix_version());
-$c = 1000000;
-$i = $t1 = $t2 = 0;
-
-$t1 = microtime(true);
-var_dump(xdebug_memory_usage() . '|' . memory_get_usage() . '|' . memory_get_usage(true));
-for($i = 0; $i < $c; ++$i)
-{
-    matrix_version();
-}
-var_dump(xdebug_memory_usage() . '|' . memory_get_usage() . '|' . memory_get_usage(true));
-$t2 = microtime(true);
-echo 'c: ' . sprintf('%.3f', $t2 - $t1) . PHP_EOL . PHP_EOL;
